2828c22a350684c5a95f134e9cb661418830d702
Natalia Nevzorova <alyaryzhaya@gmail.com>
2022-09-10 21:46:02 +0300

    rename project

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 120000;
+        int perMile =  20;
+
+        int bonusMile = ticket / perMile;
+        System.out.println(bonusMile);
+    }
+}
